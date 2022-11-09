34e4246251a0865d33057c5eaf3f2c5fda96cd10
leo <hshs345678@gmail.com>
2022-11-10 01:45:39 +0530

    finally

--- server.js
+++ server.js
@@ -22,13 +22,14 @@ app.use(cookieParser());
 //GET
 app.get("/", middleware.auth, async (req, res) => {
   try {
-    if (res.authenticated) {
-      const userEmail = res.userEmail;
-      const Data = await Model.find({ userEmail });
-      return res.render("home", { Data, title: res.username });
-    } else {
-      res.render("input", { message: null, formType: "login" });
-    }
+     // if (res.authenticated) {
+        // const userEmail = res.userEmail;
+        // const Data = await Model.find({ userEmail });
+        // return res.render('home', { Data, title: res.username });
+        // } else {
+
+        // }
+        res.render('input', { message: null, formType: 'login' });
   } catch (err) {
     console.log(err);
     return res.send(err);
